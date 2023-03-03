1cc883bc1c9205c7a7fe58fd36bf41ab8cf6989a
tannuchoudhary <tannuchoudhary10@gmail.com>
2023-03-03 20:39:40 +0530

    Change project structure to basic structure

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,10 @@
 package com.example.demo;
 
-import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
@@ -23,6 +20,5 @@ public class DemoApplication {
 	public List<String> hello(){
 		return List.of("Hello", "world");
 	}
-
 
 }
