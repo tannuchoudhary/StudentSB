0e0dcc42fba6590a08ac446b139babfbdd3e994a
tannuchoudhary <tannuchoudhary10@gmail.com>
2023-03-09 23:30:27 +0530

    Map student class to a table in db

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,28 +1,26 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
+
 
 @RestController
 @RequestMapping(path = "api/v1/student")
 public class StudentController {
+
+    private final StudentService studentService;
+    @Autowired
+    public StudentController(StudentService studentService){
+        this.studentService = studentService;
+    }
+
     @GetMapping
     public List<Student> getStudents(){
-
-        return List.of(
-                new Student(
-                        1L,
-                        "Tannu",
-                        "tannu@gmail.com",
-                        LocalDate.of(1999, Month.JANUARY, 10),
-                        23
-                )
-        );
+        return studentService.getStudents();
     }
 
 }
