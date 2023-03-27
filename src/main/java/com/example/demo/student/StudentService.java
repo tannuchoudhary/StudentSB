a0248becee068885e83c473af3ed65019dd2a250
tannuchoudhary <tannuchoudhary10@gmail.com>
2023-03-28 00:44:57 +0530

    implement n tier architecture

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,23 +1,36 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
-    public List<Student> getStudents(){
 
-        return List.of(
-                new Student(
-                        1L,
-                        "Tannu",
-                        "tannu@gmail.com",
-                        LocalDate.of(1999, Month.JANUARY, 10),
-                        23
-                )
-        );
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+    public List<Student> getStudents(){
+        return studentRepository.findAll();
+
+    }
+
+    public void addNewStudent(Student student) {
+        Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
+        if(studentOptional.isPresent()){
+            throw new IllegalStateException("email taken");
+        }
+        studentRepository.save(student);
+    }
+
+    public void deleteStudent(Long studentId) {
+
     }
 }
